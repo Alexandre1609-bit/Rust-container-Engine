a31f13083aabd88e06c480e4a9027b8e587b4443
Alexandre1609-bit <alexandre.regnier59@live.fr>
2026-03-02 21:10:12 +0100

    feat: create hostname file

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,8 @@ fn init_rootfs() {
     for dir in &folder_to_create {
         fs::create_dir_all(dir).expect("An error occured while creating the folder")
     }
+
+    let add_content = "Lorem Ipsum\n";
+    fs::write("rootfs/etc/hostname", add_content).expect("An error occured while creatin the file")
 }
+//Next: add chroot with std::os::unix::fs::chroot et std::env
